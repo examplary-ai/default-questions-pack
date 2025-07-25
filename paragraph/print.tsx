0149a2d325dae7972493b98fdf2e19c4f1bcf524
Thomas Schoffelen <thomas@schof.co>
2025-07-25 11:11:28 +0100

    chore: swap print components

--- paragraph/print.tsx
+++ paragraph/print.tsx
@@ -1,5 +1,1 @@
-export default ({ answerBoxes }) => {
-  if (!answerBoxes) return null;
-
-  return <div className="box h-12 print-no-break" />;
-};
+export default () => null;
--- single-line-text/print.tsx
+++ single-line-text/print.tsx
@@ -1,1 +1,5 @@
-export default () => null;
+export default ({ answerBoxes }) => {
+  if (!answerBoxes) return null;
+
+  return <div className="box h-12 print-no-break" />;
+};
