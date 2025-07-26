672ae550902ffd77060efbdb23aad28f2769a32b
Thomas Schoffelen <thomas@schof.co>
2025-07-26 23:01:52 +0100

    feat: add translations

--- single-line-text/component-assessment.tsx
+++ single-line-text/component-assessment.tsx
@@ -1,10 +1,11 @@
 import { FrontendAssessmentComponent, RichTextField } from "@examplary/ui";
 
-const AssessmentComponent: FrontendAssessmentComponent = ({ answer, saveAnswer }) => {
+const AssessmentComponent: FrontendAssessmentComponent = ({ answer, saveAnswer, t }) => {
   return (
     <RichTextField
       singleLine
       value={(answer?.value as string) || ""}
+      placeholder={t("placeholder")}
       onChange={(value) => {
         const valid = value.trim().length > 0;
         saveAnswer({ value, completed: valid });
