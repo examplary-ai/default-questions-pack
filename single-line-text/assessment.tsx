d9f857d28386776401d837357cc2ebf211f45f97
Thomas Schoffelen <thomas@schof.co>
2025-07-26 13:48:51 +0100

    fix: import UI component

--- single-line-text/assessment.tsx
+++ single-line-text/assessment.tsx
@@ -1,17 +1,14 @@
 import { RichTextField } from "@examplary/ui";
-// TODO: export this component
-// TODO: update to use className -> editorClassName
-// TODO: update to return empty string if isEmpty(value)
 
 export default ({ answer, saveAnswer }) => {
   return (
     <RichTextField
+      singleLine
       value={(answer?.value as string) || ""}
       onChange={(value) => {
         const valid = value.trim().length > 0;
         saveAnswer({ value, completed: valid });
       }}
-      className="box p-3 text-sm"
     />
   );
 };
