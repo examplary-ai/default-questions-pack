38594eab6055016416f793d687ec2f9667593b3a
Thomas Schoffelen <thomas@schof.co>
2025-09-26 11:44:11 +0100

    chore: remove deprecated option

--- multiple-choice-single-answer/component-print.tsx
+++ multiple-choice-single-answer/component-print.tsx
@@ -5,7 +5,7 @@ export default ({ question }) => {
     <div className="flex flex-col gap-2 mt-2">
       {(question.settings.options || []).map((option, index) => (
         <div key={index} className="flex gap-2 items-center w-full">
-          <div className="aspect-square size-5 rounded-full border-2 border-black" />
+          <div className="aspect-square size-5 rounded-full border border-black" />
           <RichTextDisplay className="flex-1">{option}</RichTextDisplay>
         </div>
       ))}
