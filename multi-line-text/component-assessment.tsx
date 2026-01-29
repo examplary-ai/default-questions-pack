c5b06bf02ac9722d5fb032be783f7104f3a0790a
Thomas Schoffelen <thomas@schof.co>
2026-01-29 11:29:24 +0000

    fix: add missing default placeholder

--- multi-line-text/component-assessment.tsx
+++ multi-line-text/component-assessment.tsx
@@ -1,9 +1,14 @@
 import { FrontendAssessmentComponent, RichTextField } from "@examplary/ui";
 
-const AssessmentComponent: FrontendAssessmentComponent = ({ answer, saveAnswer }) => {
+const AssessmentComponent: FrontendAssessmentComponent = ({
+  answer,
+  saveAnswer,
+  t,
+}) => {
   return (
     <RichTextField
       value={(answer?.value as string) || ""}
+      placeholder={t("placeholder")}
       onChange={(value) => {
         const valid = value.trim().length > 0;
         saveAnswer({ value, completed: valid });
