56163d17be362c36f89bc0ed2e27c0ae1737ff56
Thomas Schoffelen <thomas@schof.co>
2026-03-07 15:43:51 +0000

    chore: cleanup print view

--- multiple-choice-multiple-answers/component-print.tsx
+++ multiple-choice-multiple-answers/component-print.tsx
@@ -1,11 +1,15 @@
 import { FrontendPrintComponent, RichTextDisplay } from "@examplary/ui";
 
-const PrintComponent: FrontendPrintComponent = ({ question }) => {
+const PrintComponent: FrontendPrintComponent = ({ question, answerBoxes }) => {
   return (
     <div className="flex flex-col gap-2 mt-2">
       {(question.settings.options || []).map((option, index) => (
         <div key={index} className="flex gap-2 items-center w-full">
-          <div className="aspect-square size-5 rounded-sm border border-black" />
+          {answerBoxes ? (
+            <div className="aspect-square size-5 rounded-sm border border-black" />
+          ) : (
+            <span>{String.fromCharCode(65 + index)}.</span>
+          )}
           <RichTextDisplay className="flex-1">{option}</RichTextDisplay>
         </div>
       ))}
--- multiple-choice-single-answer/component-print.tsx
+++ multiple-choice-single-answer/component-print.tsx
@@ -1,14 +1,22 @@
-import { RichTextDisplay } from "@examplary/ui";
+import { FrontendPrintComponent, RichTextDisplay } from "@examplary/ui";
 
-export default ({ question }) => {
+const PrintComponent: FrontendPrintComponent = ({ question, answerBoxes }) => {
   return (
     <div className="flex flex-col gap-2 mt-2">
-      {(question.settings.options || []).map((option, index) => (
-        <div key={index} className="flex gap-2 items-center w-full">
-          <div className="aspect-square size-5 rounded-full border border-black" />
-          <RichTextDisplay className="flex-1">{option}</RichTextDisplay>
-        </div>
-      ))}
+      {(question.settings.options || []).map(
+        (option: string, index: number) => (
+          <div key={index} className="flex gap-2 items-center w-full">
+            {answerBoxes ? (
+              <div className="aspect-square size-5 rounded-full border border-black" />
+            ) : (
+              <span>{String.fromCharCode(65 + index)}.</span>
+            )}
+            <RichTextDisplay className="flex-1">{option}</RichTextDisplay>
+          </div>
+        ),
+      )}
     </div>
   );
 };
+
+export default PrintComponent;
