23812fc400aa3c5b61ccef0530ce02642cf7f929
Thomas Schoffelen <thomas@schof.co>
2026-05-08 13:23:12 +0100

    chore(matching): clean up print view

--- matching/component-print.tsx
+++ matching/component-print.tsx
@@ -50,7 +50,7 @@ const PrintComponent: FrontendPrintComponent = ({ question, t }) => {
             data-type="matching-option"
           >
             <div
-              className="flex-1 w-full border border-black rounded-xl p-2 px-3 text-sm min-w-10 min-h-10 overflow-hidden"
+              className="flex-1 w-full border border-black p-2 px-3 text-xs min-w-10 min-h-10 overflow-hidden"
               key={index}
             >
               <RichTextDisplay>{option}</RichTextDisplay>
@@ -60,15 +60,15 @@ const PrintComponent: FrontendPrintComponent = ({ question, t }) => {
                 !horizontal ? "h-0.25 w-5 bg-black" : "w-0.25 h-5 bg-black"
               }
             />
-            <div className="flex-1 w-full border border-black border-dashed rounded-xl p-2 px-3 min-w-10 min-h-10" />
+            <div className="flex-1 w-full border border-black border-dashed p-2 px-3 min-w-10 min-h-10" />
           </div>
         ))}
       </div>
-      <p className="mt-6 mb-2 text-sm">{t("possible-answers")}</p>
+      <p className="mt-6 mb-2 text-xs">{t("possible-answers")}</p>
       <div className="flex flex-wrap items-start justify-start gap-2">
         {rightItems.map((item, index) => (
           <div
-            className="border border-black rounded-xl p-2 px-3 text-sm min-w-10 min-h-10 overflow-hidden"
+            className="border border-black p-2 px-3 text-xs min-w-10 min-h-10 overflow-hidden"
             key={index}
           >
             <RichTextDisplay>{item}</RichTextDisplay>
