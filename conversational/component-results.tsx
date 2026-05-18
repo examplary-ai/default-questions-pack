d43167b17154231568deca1475271dbe155708b8
Thomas Schoffelen <thomas@schof.co>
2026-05-18 14:31:48 +0100

    chore(conversational): transcript style results

--- conversational/component-results.tsx
+++ conversational/component-results.tsx
@@ -1,4 +1,19 @@
-import { ChatView, FrontendResultsComponent } from "@examplary/ui";
+import {
+  ChatView,
+  cn,
+  FrontendResultsComponent,
+  RichTextDisplay,
+} from "@examplary/ui";
+import { BotIcon, CheckCircle2Icon, SparkleIcon, UserIcon } from "lucide-react";
+
+type Chat = {
+  role: "user" | "assistant" | "system";
+  hidden?: boolean;
+  content: string;
+  reason?: string;
+  completed?: boolean;
+  completionReason?: string;
+}[];
 
 const ResultsComponent: FrontendResultsComponent = ({ answer, t }) => {
   if (!answer.context?.chat)
@@ -8,7 +23,58 @@ const ResultsComponent: FrontendResultsComponent = ({ answer, t }) => {
       </span>
     );
 
-  return <ChatView messages={answer.context.chat} showTeacherContext />;
+  return (
+    <div className="flex flex-col gap-3">
+      {(answer.context.chat as Chat)
+        .filter(({ role, hidden }) => role !== "system" && !hidden)
+        .map((item, index) => (
+          <div key={index}>
+            <div className="flex gap-3">
+              <div>
+                {item.role === "user" ? (
+                  <UserIcon className="size-4" />
+                ) : (
+                  <BotIcon className="size-4" />
+                )}
+              </div>
+              <div
+                className={cn(
+                  "flex-1 text-sm border rounded-3xl rounded-tl-sm px-4 py-3",
+                  "animate-in slide-in-from-top",
+                  item.role === "user" &&
+                    "border-transparent bg-chat-background",
+                  item.role !== "user" &&
+                    "bg-white border-border text-zinc-700",
+                )}
+              >
+                <RichTextDisplay>{item.content}</RichTextDisplay>
+              </div>
+            </div>
+            {item.reason ? (
+              <div
+                key={`reason-${index}`}
+                className={cn("text-xs text-gray-500 mb-1 ml-7 mt-1")}
+              >
+                <span className="font-semibold">{t("reasoning")}:</span>{" "}
+                {item.reason}
+              </div>
+            ) : null}
+            {item.completed && item.completionReason ? (
+              <div className={cn("text-xs text-gray-500 mb-1 ml-7")}>
+                <span className="font-semibold text-emerald-600">
+                  <CheckCircle2Icon
+                    className="size-3 -mt-0.5 inline-block mr-1"
+                    strokeWidth={2.6}
+                  />
+                  {t("completion-reasoning")}:
+                </span>{" "}
+                {item.completionReason}
+              </div>
+            ) : null}
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default ResultsComponent;
